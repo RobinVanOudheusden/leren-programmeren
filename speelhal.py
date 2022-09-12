515b5e958e7f44639618c6c1d8e020c70ba0e7e6
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2022-09-12 09:32:05 +0200

    Afgerond

--- speelhal.py
+++ speelhal.py
@@ -1,8 +1,11 @@
 ticket_pp = 7.45
-aantal_personen = 4
-aantal_min_in_VR = 45
+aantal_personen = int(input('Met hoeveel personen bent u? '))
+aantal_min_in_VR = int(input('Hoeveel minuten wilt u in de VR? '))
 VR_kost = 0.37
 
-totale_prijs = ticket_pp * aantal_personen + (aantal_min_in_VR / 9)  * VR_kost * aantal_personen
+totale_prijs = ticket_pp * aantal_personen + aantal_min_in_VR * (VR_kost / 5 ) * aantal_personen
+float = totale_prijs
+format_float = "{:.2f}".format(float)
 
-print("Dit geweldige dagje met " + str(aantal_personen) + " mensen in de Speelhal met " + str(aantal_min_in_VR) + " minuten VR kost je maar €" + str(totale_prijs))
+
+print("Dit geweldige dagje met " + str(aantal_personen) + " mensen in de Speelhal met " + str(aantal_min_in_VR) + " minuten VR kost je maar €" + str(float))

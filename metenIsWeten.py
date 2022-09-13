f2f8a65b560a595c827da565223a92af2fd4a349
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2022-09-13 09:26:47 +0200

    metenIsWeten

---
+++ metenIsWeten.py
@@ -0,0 +1,19 @@
+a=input ("getal: ")
+b=input ("getal: ")
+
+if a>b : 
+    max = a
+    min = b
+    print("a is het grootste getal: ",max,"")
+
+elif a<b :
+    min = a
+    max = b
+    print("a is het kleinste getal ",min,"")
+
+else :
+    a==b
+    print ("a en b zijn even groot")
+
+print ("het minimum is ",min,"")
+print ("het maximum is",max,"")

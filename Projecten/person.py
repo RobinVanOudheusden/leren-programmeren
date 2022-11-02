c50a6e6df7e3a6ac93d68c6a4349a6202f2542e6
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2022-11-02 09:40:19 +0100

    deTijd

---
+++ Projecten/person.py
@@ -0,0 +1,11 @@
+Naam = input('Naam? | ')
+Adres = input('Adres? | ')
+Postcode = input('Postcode? | ')
+Woonplaats = input('Woonplaats? | ')
+
+print('----------------------------------------------------')
+print('|  Naam      : ' + Naam)              
+print('|  Adres     : ' + Adres)         
+print('|  Postcode  : ' + Postcode)                    
+print('|  Woonplaats: ' + Woonplaats)               
+print('----------------------------------------------------')

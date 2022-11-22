b5bac8be27f8d02690389212506114dd35c05b29
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2022-11-22 13:04:28 +0100

    -

--- Testing/testing.py
+++
@@ -1,7 +0,0 @@
-lijst=[]
-for x in range (20):
-    vraag = input("welk getal kiest u : ")
-    lijst.append(vraag)
-lijst.sort()
-print ("kleinste getal is ",lijst[0])
-print ("grootste getal is ",lijst[19])

340bd9dad9f9dcdfdb85531fad0a0afc8b5f91d8
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2022-12-12 13:31:40 +0100

    penis

--- robotarm-python-2021-main/robotArmOpdrachten/opdracht12.py
+++ robotarm-python-2021-main/robotArmOpdrachten/opdracht12.py
@@ -4,7 +4,7 @@ robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
 
-z = 0
+
 y = 1
 
 for a in range(8):
@@ -13,11 +13,11 @@ for a in range(8):
 for b in range(9):
     robotArm.grab()
     color = robotArm.scan()
-    z = z + 1
-    y = y + 1
 
+    y = y + 1
+    
     if color == "red":
-        for c in range(z):
+        for c in range(y-1):
             robotArm.moveRight()
         robotArm.drop()
         for d in range(y):

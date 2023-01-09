e8f8d057f12ae5b36edb60cedd6f5fbd5797619e
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-01-09 10:14:22 +0100

    raadEens

--- Projecten/raadEens.py
+++ Projecten/raadEens.py
@@ -5,17 +5,17 @@ ronde = 0
 
 while ronde < rondes:
     getal = random.randint(0, 1000)
-    print("Welkom bij 'RAAD HET GETAL'")
+    print("\nWelkom bij 'RAAD HET GETAL'")
     print("In deze game ga je een getal raden tussen 1 en 1000.")
     print("Als je binnen een radius van 50 van het getal zit, krijg je de melding 'Je bent warm!' Zit je binnen een radius van 20 van het getal krijg je de melding 'Je bent heel warm!'")
     print("Het doel van de game is dat je zoveel mogelijk punten behaald binnen 20 rondes, succes!\n")
-
+    print(getal)
     pogingen = 10
     while pogingen > 0:
         geraden = int(input("Welk getal kies je?\n-------------------\n"))
         if geraden == getal:
-            print("Gefeliciteerd! Je hebt het getal geraden.")
             punten += 1
+            print(f"Gefeliciteerd! Je hebt het getal geraden.\nPunten: {punten}")
             restart = input("Wil je door spelen? (y/n)\n")
             if restart == "y":
                 break
@@ -33,14 +33,10 @@ while ronde < rondes:
             print("Je bent warm!")
 
         ronde += 1
-        print(f"Ronde: {ronde}")
-
+        pogingen -= 1
         print(f"Punten: {punten}")
 
-        pogingen -= 1
-        print(f"Pogingen over: {pogingen}")
-
-        if ronde == 10: 
+        if pogingen == 0: 
             ronde10 = input("U heeft nu 10 rondes gehad. Wilt u nog verder? (y/n)\n")
             if ronde10 == "n":
                 exit()

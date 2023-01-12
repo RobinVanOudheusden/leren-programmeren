afe583454adbca34f699c38e9d3619af79125e15
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-01-12 13:42:28 +0100

    boodschappenLijst

---
+++ Projecten/boodschappenLijst.py
@@ -0,0 +1,22 @@
+repeat = True
+boodschappenlijst = {}
+
+while repeat == True:
+    toevoegen = input("Wat wilt u toevoegen aan uw boodschappenlijstje?\n").upper()
+    aantal = int(input(f"Hoe vaak wilt u {toevoegen} aan uw lijstje toevoegen?\n"))
+
+    if toevoegen not in boodschappenlijst:
+        boodschappenlijst.update({toevoegen:aantal})
+
+    else:
+        (boodschappenlijst)[toevoegen] += (aantal)
+
+    restart = input("Wilt u nog wat toevoegen?: (y/n)\n")
+
+    if restart == ("y"):
+        repeat = True
+    else:
+        break
+
+for aantal, toevoegen in boodschappenlijst.items():
+    print(f"{toevoegen} x {aantal}")

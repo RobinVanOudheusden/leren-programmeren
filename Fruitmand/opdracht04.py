daf72681d6011f24bd585c04a5c3cd683071f2d3
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-01-18 10:09:36 +0100

    -

--- Fruitmand/opdracht04.py
+++ Fruitmand/opdracht04.py
@@ -1,4 +1,9 @@
 from fruitmand import fruitmand
 import random
 
-aantal = input("Geef een getal: ")
+aantal = int(input("Geef een getal: "))
+
+keuze = random.choice(fruitmand)
+
+for z in range(aantal):
+    print(keuze['name'])

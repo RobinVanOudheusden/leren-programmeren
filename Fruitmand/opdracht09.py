612abd4a50b70ce7bd17dfd958940a2c33cebd5d
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-01-23 13:47:14 +0100

    -

--- Fruitmand/opdracht09.py
+++ Fruitmand/opdracht09.py
@@ -1,1 +1,12 @@
 from fruitmand import fruitmand
+
+kleurenlist = []
+for fruit in fruitmand:
+    if fruit['name']=='druif':
+        fruitmand.remove(fruit)
+
+for i in range(len(fruitmand)):
+    if fruitmand[i]['color'] not in kleurenlist:
+        kleurenlist.append(fruitmand[i]['color'])
+        
+print(kleurenlist)

e2e1f3dab8b31c026d9a7bf77498c69ed2927fc3
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-01-23 15:27:40 +0100

    -

--- Fruitmand/opdracht08.py
+++ Fruitmand/opdracht08.py
@@ -1,7 +1,15 @@
 from fruitmand import fruitmand
 
+totaal_gewicht = 0
+
 fruitmand.append({
     'name' : 'watermeloen',
-    'weight' : 2900,
+    'weight' : 2800,
     'color' : 'red',
     'round' : True})
+
+for fruit in range(len(fruitmand)):
+    gewicht = fruitmand[fruit]['weight']
+    totaal_gewicht = totaal_gewicht + gewicht
+
+print(f"Totaal gewicht fruit: ",{totaal_gewicht})
---
+++ Fruitmand/opdracht10.py
@@ -0,0 +1,8 @@
+from fruitmand import fruitmand
+
+lijst = {}
+
+for i in range(len(fruitmand)):
+    lijst.update({fruitmand[i].get('weight')/1000: fruitmand[i].get('name')})
+
+print(sorted(lijst.items(), reverse=True))

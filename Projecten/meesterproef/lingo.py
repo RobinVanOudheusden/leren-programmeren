fc8f8422c579afa96469709fd9b5970b1ff90caf
RobinVanOudheusden <112872296+RobinVanOudheusden@users.noreply.github.com>
2023-07-03 11:53:16 +0200

    -

--- Projecten/meesterproef/lingo.py
+++ Projecten/meesterproef/lingo.py
@@ -3,7 +3,7 @@ from lingoWords import words
 from termcolor import colored
 
 def get_random_word(words):
-    return random.choice(words)
+    return random.choice(words) 
 
 def get_first_letter(random_woord):
     eerste_letter = random_woord[0]
@@ -13,14 +13,14 @@ while True:
     print('Welkom bij Lingo!\n')
     print("Voer een 5-letterwoord in en druk op Enter!")
     print("Je krijgt 5 pogingen om het woord te raden.")
-    print("Als je een letter goed hebt, maar niet op de juiste plek, dan wordt de letter geel.")
+    print("Als de letter in het woord zit, maar niet op de juiste plek staat, dan wordt de letter geel.")
     print("Als je een letter goed hebt, en op de juiste plek, dan wordt de letter groen.")
     print("Als je een letter fout hebt en de letter zit niet in het woord, dan wordt de letter rood.\n")
 
     woord = get_random_word(words)
     eerste_letter = get_first_letter(woord)
     print("Het woord begint met de letter: " + eerste_letter)
-    print(woord)
+    # print(woord)
 
     for poging in range(1,6):
         while True:
@@ -30,19 +30,25 @@ while True:
             else:
                 print("Het woord moet 5 letters lang zijn!")
 
-        correcte_letters = ""
+        correcte_letters = list("_"*5)
+        temp_woord = list(woord)
 
+        # Eerste loop voor de groene letters
         for i in range(5):
             if raden[i] == woord[i]:
-                correcte_letters += colored(raden[i], 'green')
-            elif raden[i] in woord:
-                correcte_letters += colored(raden[i], 'yellow')
-            elif raden[i] not in woord:
-                correcte_letters += colored(raden[i], 'red')
-            else:
-                 correcte_letters += raden[i]
+                correcte_letters[i] = colored(raden[i], 'green')
+                temp_woord[i] = '_'
 
-        print(correcte_letters)
+        # Tweede loop voor de gele en rode letters
+        for i in range(5):
+            if correcte_letters[i] == "_":
+                if raden[i] in temp_woord:
+                    correcte_letters[i] = colored(raden[i], 'yellow')
+                    temp_woord[temp_woord.index(raden[i])] = '_'
+                else:
+                    correcte_letters[i] = colored(raden[i], 'red')
+
+        print(''.join(correcte_letters))
 
         if raden == woord:
             print(colored(f"Gefeliciteerd! Je hebt het woord geraden in poging {poging}!", 'red'))
